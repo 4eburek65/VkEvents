b9a8224a9f5a5f4fd1b5565a019f62c48b665df5
4eburek65 <50070384+4eburek65@users.noreply.github.com>
2023-05-10 18:33:25 +0300

    Update db_params.php

--- include/db_params.php
+++ include/db_params.php
@@ -1,7 +1,7 @@
 <?php
 $params = [
-	'host' => 'localhost',
-    'dbname' => 'Events',
-    'user' => 'root',
-    'password' => '',     
+	'host' => $_ENV["DB_HOST"],
+	'dbname' => $_ENV["DB_NAME"],
+	'user' => $_ENV["DB_USER"],
+	'password' => $_ENV["DB_PASSWORD"],    
 ];
