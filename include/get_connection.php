d1c2b8dbdb9e669b3101deb7bdc6b6e5fc73ac63
4eburek65 <50070384+4eburek65@users.noreply.github.com>
2023-05-10 18:36:26 +0300

    Update get_connection.php

--- include/get_connection.php
+++ include/get_connection.php
@@ -4,7 +4,7 @@ function getConnection()
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/include/db_params.php';
 	static $connection = null;
 	if (null === $connection) {
-		$connection = mysqli_connect($params['host'], $params['user'], $params['password'], $params['dbname']);
+		$connection = mysqli_connect($params['host'], $params['user'], $params['password'], $params['dbname'], $params['port']);
 	}
 	return $connection;
 }
